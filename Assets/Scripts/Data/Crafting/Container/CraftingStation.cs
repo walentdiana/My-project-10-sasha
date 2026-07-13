091d4c77c66bbbccf8ec3ffa60433eba25ba533e
Dziana <walent17072003@gmail.com>
2026-07-14 00:26:09 +0200

    craft ui 38 15

--- Assets/Scripts/Data/Crafting/Container/CraftingStation.cs
+++ Assets/Scripts/Data/Crafting/Container/CraftingStation.cs
@@ -8,64 +8,63 @@ namespace Data.Crafting.Container
     [CreateAssetMenu(fileName = "New Crafting Station", menuName = "Crafting System/Station")]
     public class CraftingStation : ScriptableObject
     {
-        public CraftingRecipeRegistry registry;
+        public CraftingRecipeRegistry database;
         public Recipe container;
-
-        public bool CanBeCrafted(RecipeSlot recipe, InventoryObject  inventory)
-        {
-            foreach (var ingredient in recipe.recipe.ItemIngredients)
-            {
-                int have = CountInInventory(inventory, ingredient);
-                    if (have < ingredient.Amount)
-                        return false;
-            }
-            return true;
-        }
-
-        private int CountInInventory(InventoryObject inventory, CraftingIngredients ingredient)
-        {
-            int total = 0;
-
-            foreach (var slot in inventory.Container.Items)
-            {
-                if (slot.ID >= 0
-                    && slot.ID != null
-                    && slot.ID == ingredient.Item.Id)
-                    total += slot.amount;
-            }
-            return total;
-        }
-
-        public bool Craft()
-        {
-            return true;
-        }
+        public CraftingStationType stationType;
     }
 
 
     [Serializable]
     public class Recipe
     {
-        public List<RecipeSlot> RecipeItems; //хранит все рецепты для конкретной станции
+        public List<RecipeSlot> RecipeItems; //хранит все доступные рецепты для конкретной станции
 
+        [field:SerializeField] public event Action<RecipeSlot> OnRecipeAdded;
+        
         public void Initialize()
         {
             RecipeItems = new List<RecipeSlot>();
         }
 
-        public void AddRecipe(RecipeSlot slot) //открывается новый рецепт за баранки
+        public void AddRecipe(RecipeSlot recipe) //открывается новый рецепт за баранки
         {
-            if(RecipeItems.Contains(slot))
+            if(RecipeItems.Contains(recipe))
                 return;
+
+            /*foreach (var existing in RecipeItems)
+            {
+                if (existing.recipe.Id == recipe.recipe.Id)
+                {
+                    return;
+                }
+            }*/
             
-            RecipeItems.Add(slot);
+            RecipeItems.Add(recipe);
+            OnRecipeAdded?.Invoke(recipe);
         }
     }
 
     [Serializable]
-    public class RecipeSlot
+    public class RecipeSlot //конкретный рецепт в конкретной ячейке в нашей системе. на какой рецепт
+        //кликнул наш пользак
     {
         public CraftingRecipe recipe;
         public CraftingStationType station;
+        public bool bIsAvailable;
+
+        [field: SerializeField] public event Action<bool> OnIsAvailableChanged;
+
+        public RecipeSlot(CraftingRecipe recipe, CraftingStationType station)
+        {
+            this.recipe = recipe;
+            this.station = station;
+        }
+
+        public void SetAvailable(bool value)
+        {
+            if(value = bIsAvailable)
+                return;
+            bIsAvailable = value;
+        }
     }
 }
